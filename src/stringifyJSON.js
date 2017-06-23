1103a7b8e935b61a82a50be6738a1e0f08df6a71
jr-gray <william_gray@alumni.brown.edu>
2017-06-23 13:27:42 -0700

    Fix spacing in stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,28 +5,30 @@
 
 var stringifyJSON = function(obj) {
 
-	if (Array.isArray(obj)) {
+  if (Array.isArray(obj)) {
+    
+  	var arr = [];
+  	for (var i=0; i<obj.length; i++) {
+  		arr.push(stringifyJSON(obj[i]));
+  	}
+  	return '[' + arr.join(',') + ']';
 
-		var arr = [];
-		for (var i=0; i<obj.length; i++) {
-			arr.push(stringifyJSON(obj[i]));
-		}
-		return '[' + arr.join(',') + ']';
+  } else if (typeof obj === 'object') {
 
-	} else if (typeof obj === 'object') {
-
-		if (obj === null) { return String(obj); }
+  	if (obj === null) { return String(obj); }
   	var res = [];
-		for (var key in obj) {
-			if (typeof obj[key] === 'function' || obj[key] === undefined) { continue; }
-			res.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
-		}
-		return '{' + res.join(',') + '}';
+  	for (var key in obj) {
+  		if (typeof obj[key] === 'function' || obj[key] === undefined) { continue; }
+  		res.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+  	}
+  	return '{' + res.join(',') + '}';
 
   } else {
+
   	if (typeof obj === 'string') {
   		return '"' + obj + '"';
   	}
   	return String(obj);
   }
+
 };
