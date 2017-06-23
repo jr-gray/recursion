f95610827d8e8e8ebc3927358cbd35240b040c33
jr-gray <william_gray@alumni.brown.edu>
2017-06-22 17:45:53 -0700

    Add stringigy and getelements

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+	var nodes = [];
+  function test(node) {
+    if (node.classList && node.classList.contains(className)) {
+      nodes.push(node);
+    }
+    
+    node.childNodes.forEach(function(child) {
+    	test(child);
+    });
+  }
+	  
+	  test(document.body);
+	    
+	  return nodes;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,29 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+	if (Array.isArray(obj)) {
+
+		var arr = [];
+		for (var i=0; i<obj.length; i++) {
+			arr.push(stringifyJSON(obj[i]));
+		}
+		return '[' + arr.join(',') + ']';
+
+	} else if (typeof obj === 'object') {
+
+		if (obj === null) { return String(obj); }
+  	var res = [];
+		for (var key in obj) {
+			if (typeof obj[key] === 'function' || obj[key] === undefined) { continue; }
+			res.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+		}
+		return '{' + res.join(',') + '}';
+
+  } else {
+  	if (typeof obj === 'string') {
+  		return '"' + obj + '"';
+  	}
+  	return String(obj);
+  }
 };
